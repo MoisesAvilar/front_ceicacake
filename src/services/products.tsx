19cce2382d4dba0d0b5572667adbad210ffe829f
Moisés <moisesavilar0@gmail.com>
2024-06-24 23:15:53 -0300

    fix layout

--- src/services/products.tsx
+++ src/services/products.tsx
@@ -1,9 +1,11 @@
 export const Products = [
-  { value: "BOLO_DE_POTE_P", label: "Bolo de pote P" },
-  { value: "BOLO_DE_POTE_G", label: "Bolo de pote G" },
+  { value: "TORTA", label: "Torta" },
   { value: "BRASINHA_COMUM", label: "Brasinha comum" },
   { value: "BRASINHA_GOURMET", label: "Brasinha gourmet" },
+  { value: "BOLO_DE_POTE_P", label: "Bolo de pote P" },
+  { value: "BOLO_DE_POTE_G", label: "Bolo de pote G" },
+  { value: "BOLO_VULCAO_P", label: "Bolo vulcão P" },
+  { value: "BOLO_VULCAO_M", label: "Bolo vulcão M" },
+  { value: "BOLO_VULCAO_G", label: "Bolo vulcão G" },
   { value: "BOLO_DE_ANIVERSARIO", label: "Bolo de aniversário" },
-  { value: "BOLO_VULCAO", label: "Bolo vulcão" },
-  { value: "TORTA", label: "Torta" },
 ];
